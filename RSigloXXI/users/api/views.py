f4d8916aa2dc8f2c782b59ba5175e3b96fff677f
alcir <aleriveracorrales@gmail.com>
2022-09-01 17:50:11 -0400

    EndPoint obtener dato cuenta logeada

--- RSigloXXI/users/api/views.py
+++ RSigloXXI/users/api/views.py
@@ -1,10 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAdminUser
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
 
 from users.models import User 
 from users.api.serializers import UserSerializer
 from django.contrib.auth.hashers import make_password
 
+#CRUD PARA MODIFICAR USUARIOS
 class UserApiViewSet(ModelViewSet):
     permission_class = [IsAdminUser]
     serializer_class = UserSerializer
@@ -23,3 +26,11 @@ class UserApiViewSet(ModelViewSet):
         else:
             request.data['password'] = request.user.password
         return super().update(selft, request, *args, **kwargs)
+
+#Clase unicamente para pedir informacion del usuario
+class UserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request ):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
