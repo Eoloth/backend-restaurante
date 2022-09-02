5ed6da11f477520f022caca6ba3554e966be6604
alcir <aleriveracorrales@gmail.com>
2022-09-02 01:33:25 -0400

    ModelViewSet de categorias

--- RSigloXXI/RSigloXXI/urls.py
+++ RSigloXXI/RSigloXXI/urls.py
@@ -21,6 +21,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 from users.api.router import router_user 
+from categories.api.router import router_category
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -43,4 +44,5 @@ urlpatterns = [
         cache_timeout=0), name='schema-redoc'),
     path('api/', include('users.api.router')),
     path('api/', include(router_user.urls)),
+    path('api/', include(router_category.urls)),
 ]
