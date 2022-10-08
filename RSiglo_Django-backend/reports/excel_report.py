49c6ebc642644ecf119c5116a19fd9cedc073b27
Ruben Manriquez <rubenmanriquezs@hotmail.com>
2022-10-08 16:25:07 -0400

    metodo reporte creado para guardar en unidad C:/ (raiz), funcionando

--- RSiglo_Django-backend/reports/excel_report.py
+++ RSiglo_Django-backend/reports/excel_report.py
@@ -1,5 +1,18 @@
 import os
 import pandas as pd
+from pandas import ExcelWriter
 
 
-ruta="/"
+ruta = "C:/"
+
+datos = pd.DataFrame({'id':[1,2,3,4],
+                      'nombre':['Juan','Pedro','Maria','Luis'],
+                      'edad':[20,30,40,50]})
+
+datos = datos[['id','nombre','edad']]
+
+writer = ExcelWriter(ruta + '/datos.xlsx')
+
+datos.to_excel(writer,'reporte',index=False)
+
+writer.save()
